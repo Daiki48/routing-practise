58ef02a538cad931eef1d06e504b7610a4a07cdb
Daiki48 <99887018+NakashimaDaiki@users.noreply.github.com>
2022-04-19 21:59:23 +0900

    ルーティング練習

---
+++ src/component/Contact.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+export const Contact = () => {
+  return (
+    <>
+    <h1>Contactページです</h1>
+    <Link to="/">Homeページに移動</Link>
+    <br />
+    <Link to="/about">Aboutページに移動</Link>
+    </>
+    
+  )
+}
